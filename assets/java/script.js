1fd506843800c0b13d2a8a56778533381b1165a6
Sadie Sial <sadiesial@gmail.com>
2021-08-11 23:47:30 -0500

    added comments to build code around.

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,20 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowerChars = "abcdefghijklmnopqrstuvwxyz"
+var alphetChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numberChars = "123456789"
 var specialChars = "!@#$%^&*+"
 var length;
 
 
 // Write password to the #password input
+// Ok here's what we need:
+// must be between 8 -128 characters
+// uppercase?
+// lowercase?
+// numeric?
+// special characters?
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+// Alerts when they mess up -_-
+// Plz enter how many digits 
+// Plz answer yes or no
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
