635f96eb5493baf59fd20ab13c6f858cbe705a66
Sadie Sial <sadiesial@gmail.com>
2021-08-13 13:26:53 -0500

    included length function + started user input vars bb

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,13 +1,13 @@
 // Assignment Code - hw
 var generateBtn = document.querySelector("#generate");
-var numsChars = "123456789";
-var alphChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numsChars = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var alphLowerChars = "abcdefghijklmnopqrstuvwxyz";
+var alphUpperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var specChars = "!@#$%^&*+";
 var passLength;
 var numsRight;
 var alphRight;
 var specRight;
-var 
 
 // Write password to the #password input -hw
 // Ok here's what we need:
@@ -17,18 +17,27 @@ var
 // numeric?
 // special characters?
 
+userInput();
+
+function userInput() {
+  var passLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters");
+    if (passLength < 8 || passLength > 128 ){
+    alert("I'm gonna need something between 8-128 characters :)");
+}
+var useLowerCase = confirm('Use LowerCase?');
+var useUpperCase = confirm('Use Uppers?');
+var useSpecials = confirm('Use Specials?');
+var useNumbahs = confirm('Use Numbers?');
+
+}
+
 function generatePassword() {
 
+}
 
-var passLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters"));
-  if ((passLength < 8 || passLength > 128 );
-    alert("I'm gonna need something between 8-128 characters :)");
-  else if {
-    numsRight = numsRight("");
-    alphRight = alphRight("");
-    specRight = specRight("");
 
-  }
+
+
 
 
 //---------- given code from hw
